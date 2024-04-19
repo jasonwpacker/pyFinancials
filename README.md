46d2d68870dcaaa7a93f9d7308c14e9754473605
Jason Packer <jasonwpacker@gmail.com>
2024-04-18 23:42:29 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Holidays and Business Days
+
+This short and dirty version will return a list of all holidays included in a configuration file
+or, provided a specific date, return the next business day taking into account weekends and the
+holidays included in the configuration file.
---
+++ pyHolidays.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+# Reformat the data from 'holidays.txt' into JSON
+@app.route('/holidays', methods=['GET'])
+def get_holidays():
+    return jsonify(holidays)
+
+# If the date submitted is a holiday or weekend, increment the date until it is not
+@app.route('/next-business-day/<date>', methods=['GET'])
+def get_next_business_day(date):
+    date = datetime.strptime(date,'%Y-%m-%d')
+    while date in holidays or date.weekday() >= 5:
+        date += timedelta(days=1)
+    return jsonify({'next_business_day': date.strftime('%Y-%m-%d')})
+
+if __name__ == '__main__':
+    with open('holidays.txt', 'r') as f:
+        holidays = [line.strip() for line in f.readlines()]
+    app.run(debug=True)
