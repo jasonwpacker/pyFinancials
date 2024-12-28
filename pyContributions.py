d06dac41c9a907fa65ff9b854983d3f82c0941ec
Jason Packer <jasonpacker@jasnmis.com>
2024-12-28 17:39:44 -0600

    Added default values if an invalid year is provided

--- pyContributions.py
+++ pyContributions.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/contribution/401k/<year>', methods=['GET'])
 def get_401k_max(year):
-    return maxima.get("401k").get(year)
+    return maxima.get("401k").get(year, '0')
 
 @app.route('/contribution/SIMPLE/<year>', methods=['GET'])
 def get_SIMPLE_max(year):
-    return maxima.get("SIMPLE").get(year)
+    return maxima.get("SIMPLE").get(year, '0')
 
 @app.route('/contribution/401kCatchUp/<year>', methods=['GET'])
 def get_401kCatchUp_max(year):
-    return maxima.get("401kCatchUp").get(year)
+    return maxima.get("401kCatchUp").get(year, '0')
 
 @app.route('/contribution/SIMPLECatchUp/<year>', methods=['GET'])
 def get_SIMPLECatchUp_max(year):
-    return maxima.get("SIMPLECatchUP").get(year)
+    return maxima.get("SIMPLECatchUP").get(year, '0')
 
 if __name__ == '__main__':
     with open('limits.json') as limitsfile:
